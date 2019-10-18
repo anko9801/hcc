90f441ac58eae8dff74979aa3b1976688d41fcaf
Anko <daiusa3@icloud.com>
2019-10-19 00:46:27 +0900

    :wrench: codegen referctaring

--- src/codegen.h
+++ src/codegen.h
@@ -17,3 +17,98 @@ typedef struct {
 } loop_info;
 
 loop_info cur_loop = {0, 0};
+
+typedef enum regs {
+	RAX,
+	RBX,
+	RCX,
+	RDX,
+	RSI,
+	RDI,
+	RBP,
+	RSP,
+	R8,
+	R9,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	R15,
+} regs;
+
+char *regs64[16] = {
+	"rax",
+	"rbx",
+	"rcx",
+	"rdx",
+	"rsi",
+	"rdi",
+	"rbp",
+	"rsp",
+	"r8",
+	"r9",
+	"r10",
+	"r11",
+	"r12",
+	"r13",
+	"r14",
+	"r15",
+};
+
+char *regs32[16] = {
+	"eax",
+	"ebx",
+	"ecx",
+	"edx",
+	"esi",
+	"edi",
+	"ebp",
+	"esp",
+	"r8d",
+	"r9d",
+	"r10d",
+	"r11d",
+	"r12d",
+	"r13d",
+	"r14d",
+	"r15d",
+};
+
+char *regs16[16] = {
+	"ax",
+	"bx",
+	"cx",
+	"dx",
+	"si",
+	"di",
+	"bp",
+	"sp",
+	"r8w",
+	"r9w",
+	"r10w",
+	"r11w",
+	"r12w",
+	"r13w",
+	"r14w",
+	"r15w",
+};
+
+char *regs8[16] = {
+	"al",
+	"bl",
+	"cl",
+	"dl",
+	"sil",
+	"dil",
+	"bpl",
+	"spl",
+	"r8b",
+	"r9b",
+	"r10b",
+	"r11b",
+	"r12b",
+	"r13b",
+	"r14b",
+	"r15b",
+};
